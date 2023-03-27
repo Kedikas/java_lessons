15b987a4a166f25120415858eff9c4b40153d858
Victoria <delviktoria5@gmail.com>
2023-03-27 19:05:06 +0300

    Second

---
+++ src/pack1/Example.java
@@ -0,0 +1,16 @@
+public class Example {
+    String name = "Vika";
+    float weight = 58.9f;
+    byte age = 21;
+    short height = 168;
+
+    public Example() {
+    }
+
+    public Example(String name, float weight, byte age, short height){
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+        this.height = height;
+    }
+}
